e5fd6521fc7aeff536bb9197f0ac8bcf8e3a8e3b
Orlando Buzana <orlandobuzana@outlook.com>
2026-04-17 12:06:37 -0400

    Initial Commit

---
+++ ArtTrace/ArtTraceApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArtTraceApp.swift
+//  ArtTrace
+//
+//  Created by Orlando Buzana on 4/17/26.
+//
+
+import SwiftUI
+
+@main
+struct ArtTraceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ArtTrace/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ArtTrace
+//
+//  Created by Orlando Buzana on 4/17/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
